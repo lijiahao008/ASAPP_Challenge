cf1700322d11bfb4bb0f1e5e71881b78db19974c
Jiahao Li <lijiahao008@gmail.com>
2017-05-17 22:57:35 -0400

    added npm and webpack scripts

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./assets/javascripts/asapp_challenge.jsx",
+  output: {
+    path: path.join(__dirname, 'bundle'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx", '.css']
+  }
+};
